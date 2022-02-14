f0261c2f5d9868cc99a2517502b9fa21873c1786
ZOUBAIRI Saad <saad.zoubairi@bordeaux-inp.fr>
2022-02-14 11:33:09 +0100

    mockito tests added

--- project/src/test/java/com/example/demo/controllerTest/ControllerTest.java
+++ project/src/test/java/com/example/demo/controllerTest/ControllerTest.java
@@ -1,24 +1,54 @@
 package com.example.demo.controllerTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
+
+import com.example.demo.controller.RegistrationController;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.UserDTO;
+import com.example.demo.repository.UsersRepository;
+import com.example.demo.service.RegistrationService;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 public class ControllerTest {
 
+    @Mock private UsersRepository usersRepo;
+    @Mock private UserMapper userMapper;
+    @Mock private RegistrationService registrationService;
+    
+    @InjectMocks RegistrationController registrationController;
+
     @Test
-    public void getUserByIdTest() {
+    public void getUserTest() {
 
+        UserDTO expectedResponse = new UserDTO("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+        Mockito.doReturn(expectedResponse).when(registrationService).getUserById("saad");
+
+        UserDTO actualResponse = registrationController.getUser("saad");
+        
+        verify(registrationService).getUserById("saad");
+
+        assertEquals(expectedResponse.getName(), actualResponse.getName());
     }
-
 
 
     @Test
     public void registerTest() {
+        UserDTO expectedResponse = new UserDTO("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+        Mockito.doNothing().when(registrationService).register(Mockito.any(UserDTO.class));
 
+        registrationController.postUser(expectedResponse);
+
+        verify(registrationService).register(Mockito.any(UserDTO.class));
     }
 
 }
--- project/src/test/java/com/example/demo/registrationTest/ServiceTest.java
+++ project/src/test/java/com/example/demo/registrationTest/ServiceTest.java
@@ -2,21 +2,33 @@ package com.example.demo.registrationTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.UserDTO;
+import com.example.demo.repository.UsersRepository;
 import com.example.demo.service.RegistrationService;
+import com.example.demo.user.User;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootTest
 public class ServiceTest {
-    
-    @Autowired private RegistrationService registrationService;
+
+    @Mock private UsersRepository usersRepo;
+    @Mock private UserMapper userMapper;
+    @Spy @InjectMocks private RegistrationService registrationService;
+            
     @Test
     public void checkNullTest() {
         ResponseStatusException thrown = assertThrows(ResponseStatusException.class, () -> registrationService.checkNull(null));
@@ -36,5 +48,47 @@ public class ServiceTest {
         assertEquals(false, registrationService.validUser(userDTO4));
     }
 
+    @Test
+    public void getUserByIdTest() {
+        
+        User response = new User("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+        UserDTO expectedResponse = new UserDTO("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+        
+        Mockito.when(usersRepo.findById(Mockito.anyString())).thenReturn(Optional.of(response));
+        Mockito.when(userMapper.userToUserDTO(Mockito.any(User.class))).thenReturn(expectedResponse);
+        Mockito.doNothing().when(registrationService).checkNull(Mockito.any(UserDTO.class));
 
+        UserDTO actualResponse = registrationService.getUserById("saad");
+
+        verify(registrationService).checkNull(Mockito.any(UserDTO.class));
+        verify(usersRepo).findById("saad");
+        verify(userMapper).userToUserDTO(Mockito.any(User.class));
+        
+        assertEquals(expectedResponse.getName(), actualResponse.getName());
+        assertEquals(expectedResponse.getBirth(), actualResponse.getBirth());
+    }
+
+    @Test
+    public void registerFailTest(){
+        UserDTO expectedResponse = new UserDTO("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+
+        Mockito.doReturn(false).when(registrationService).validUser(Mockito.any(UserDTO.class));
+
+        registrationService.register(expectedResponse);
+        verify(usersRepo, never()).save(Mockito.any(User.class));
+    }
+
+    @Test
+    public void registerSuccessTest(){
+
+        User response = new User("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+        UserDTO expectedResponse = new UserDTO("saad", LocalDate.of(2000, 04, 05), "France", null, null);
+
+        Mockito.doReturn(true).when(registrationService).validUser(Mockito.any(UserDTO.class));
+        Mockito.when(userMapper.userDTOToUser(Mockito.any(UserDTO.class))).thenReturn(response);
+        Mockito.when(usersRepo.save(Mockito.any(User.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        registrationService.register(expectedResponse);
+        verify(usersRepo).save(Mockito.any(User.class));
+    }
 }
